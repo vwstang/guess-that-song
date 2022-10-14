e1f242610fcda3705076d05f29315625fd337c7a
vwstang <vwstang6@gmail.com>
2022-10-13 23:14:15 -0400

    Clean out old songs now unavailable from API

--- js/app.js
+++ js/app.js
@@ -2,139 +2,142 @@
 // Namespace for all logic related to requesting information from APIs.
 const apiRequest = {};
 
-apiRequest.apiKey = "4f71dc3a502c751a4c0164871f365fde"; // musixmatch account is under Vincent
-apiRequest.apiURL = "https://api.musixmatch.com/ws/1.1/";
+apiRequest.apiKey = '4f71dc3a502c751a4c0164871f365fde'; // musixmatch account is under Vincent
+apiRequest.apiURL = 'https://api.musixmatch.com/ws/1.1/';
 
 //==-- THIS IS USED TO SEARCH FOR SONG NAMES TO GET OUR TRACK IDS TO IMPLEMENT INTO THE GAME BECAUSE I HAVE GOOD AT GRAMMAR... ENGRISH --==//
 apiRequest.getLyricsIDList = () => {
   $.ajax({
     url: `${apiRequest.apiURL}track.search`,
-    type: "GET",
-    dataType: "jsonp",
+    type: 'GET',
+    dataType: 'jsonp',
     jsonpCallback: 'jsonp_callback',
     contentType: 'application/json',
     data: {
       apikey: apiRequest.apiKey,
-      format: "jsonp",
-      callback: "jsonp_callback",
+      format: 'jsonp',
+      callback: 'jsonp_callback',
       f_has_lyrics: 1,
       f_is_instrumental: 0,
       // q_artist: "eminem",
       f_artist_id: 426,
-      q_track: "the real slim shady", // REPLACE THIS TO FIND OUT THE TRACK_ID
+      q_track: 'the real slim shady', // REPLACE THIS TO FIND OUT THE TRACK_ID
       // f_track_release_group_first_release_date_max: 20180101,
-      page_size: 100
-    }
-  }).then(res => {
+      page_size: 100,
+    },
+  }).then((res) => {
     apiRequest.lyricsIDList = res.message.body.track_list;
     console.log(apiRequest.lyricsIDList); // RESULT OF THIS CONSOLE LOG GETS ADDED INTO compatibleSongs object in data.js
   });
-}
+};
 
-
-apiRequest.getLyrics = trackID => {
+apiRequest.getLyrics = (trackID) => {
   return $.ajax({
     url: `${apiRequest.apiURL}track.lyrics.get`,
-    type: "GET",
-    dataType: "jsonp",
+    type: 'GET',
+    dataType: 'jsonp',
     jsonpCallback: 'jsonp_callback',
     contentType: 'application/json',
     data: {
       apikey: apiRequest.apiKey,
-      format: "jsonp",
-      callback: "jsonp_callback",
-      track_id: trackID
-    }
+      format: 'jsonp',
+      callback: 'jsonp_callback',
+      track_id: trackID,
+    },
   });
-}
+};
 
-apiRequest.getTrack = trackID => {
+apiRequest.getTrack = (trackID) => {
   return $.ajax({
     url: `${apiRequest.apiURL}track.get`,
-    type: "GET",
-    dataType: "jsonp",
+    type: 'GET',
+    dataType: 'jsonp',
     jsonpCallback: 'jsonp_callback2',
     contentType: 'application/json',
     data: {
       apikey: apiRequest.apiKey,
-      format: "jsonp",
-      callback: "jsonp_callback2",
-      track_id: trackID
-    }
+      format: 'jsonp',
+      callback: 'jsonp_callback2',
+      track_id: trackID,
+    },
   });
-}
+};
 
 //==-- OBJECT NAMESPACE: app --==//
 // Stores logic behind behaviour of the entire app
 const app = {};
 
-app.printHTML = (id, tag, text) => $(`#${id}`).append(`<${tag}>${text}</${tag}>`);
-
+app.printHTML = (id, tag, text) =>
+  $(`#${id}`).append(`<${tag}>${text}</${tag}>`);
 
 app.init = () => {
-  $("form").on("submit", event => {
+  $('form').on('submit', (event) => {
     event.preventDefault();
     game.answerCheck();
   });
-  
-  $("#startButton").on("click", () => {
-    $("#splashPage").toggleClass("hide");
-    $("#gamePage").toggleClass("hide");
-    game.difficultyLevel = $("input[name=difficulty]:checked").val();
+
+  $('#startButton').on('click', () => {
+    $('#splashPage').toggleClass('hide');
+    $('#gamePage').toggleClass('hide');
+    game.difficultyLevel = $('input[name=difficulty]:checked').val();
     game.startQuestion();
     game.startTimer();
   });
 
-  $("#settingsButton").on("click", () => {
-    $("#settingsPage").toggleClass("hide");
-    $("#exitSettings").focus();
+  $('#settingsButton').on('click', () => {
+    $('#settingsPage').toggleClass('hide');
+    $('#exitSettings').focus();
   });
 
-  $("#exitSettings").on("click", () => {
-    $("#settingsPage").toggleClass("hide");
-    $("#settingsButton").focus();
+  $('#exitSettings').on('click', () => {
+    $('#settingsPage').toggleClass('hide');
+    $('#settingsButton').focus();
   });
 
-  $("#leaderboardButton").on("click", () => {
-    $("#leaderboardPopup").toggleClass("hide");
-    $("#exitLeaderboard").focus();
-    
+  $('#leaderboardButton').on('click', () => {
+    $('#leaderboardPopup').toggleClass('hide');
+    $('#exitLeaderboard').focus();
+
     const topFivePlayers = leaderboard.getTopFive();
 
     // console.log(topFivePlayers);
 
     topFivePlayers.forEach((entry, index) => {
-      app.printHTML("popup-entries", "p class=\"popup-entry\"", `${index + 1}) ${entry[0]} (Score: ${entry[1]})`);
+      app.printHTML(
+        'popup-entries',
+        'p class="popup-entry"',
+        `${index + 1}) ${entry[0]} (Score: ${entry[1]})`
+      );
     });
   });
 
-  $("#exitLeaderboard").on("click", () => {
-    $("#popup-entries").empty();
-    $("#leaderboardPopup").toggleClass("hide");
-    $("#leaderboardButton").focus();
+  $('#exitLeaderboard').on('click', () => {
+    $('#popup-entries').empty();
+    $('#leaderboardPopup').toggleClass('hide');
+    $('#leaderboardButton').focus();
   });
 
-  $("#hintButton").on("click", () => {
+  $('#hintButton').on('click', () => {
     if (game.currQuestion.length !== 0) {
-      app.printHTML("lyrics", "p", game.currQuestion.shift());
+      app.printHTML('lyrics', 'p', game.currQuestion.shift());
     }
   });
 
-  $("#passButton").on("click", () => {
+  $('#passButton').on('click', () => {
     game.resetQuestion();
     game.startQuestion();
   });
 
-  $("#exitButton").on("click", () => {
+  $('#exitButton').on('click', () => {
     game.endGame();
   });
-}
+};
 
 $(() => {
   app.init();
 
   // Save the leaderboard on value updates
-  leaderboard.db.on("value", dbSnapshot => {
+  leaderboard.db.on('value', (dbSnapshot) => {
     leaderboard.snapshot = dbSnapshot.val();
     // console.log(leaderboard.snapshot);
   });
--- js/data.js
+++ js/data.js
@@ -2,16 +2,16 @@
 // This object stores artist names as keys with values of arrays, which consist of track_ids used for musixmatch API call to get lyrics.
 // This is required because the musixmatch API has data on live versions, covers, remixes, etc, which we don't want to pick up in the game, but no readily available way of filtering these out according to their API
 const questionLibrary = {
-  Gotye: [
-    15953433 // Somebody That I Used To Know
-  ],
+  // Gotye: [
+  //   15953433 // Somebody That I Used To Know
+  // ],
   // 13958599
-  "Ariana Grande": [
+  'Ariana Grande': [
     160369747, // Thank u, next
     155480782, // God is a woman
     155480787, // No Tears Left To Cry
-    124488135, // Side To Side
-    155480786 // Breathin
+    // 124488135, // Side To Side
+    155480786, // Breathin
   ],
   // 33491453
   Drake: [
@@ -19,109 +19,70 @@ const questionLibrary = {
     153434470, // God's Plan
     114292344, // One Dance
     152383631, // In My Feelings
-    111790600 // Views
+    111790600, // Views
   ],
   // 259675
-  "Taylor Swift": [
-    74443483, // Style
-    74443482, // Blank Space
-    17170438, // Fifteen
-    74274886, // Shake It Off
-    31326753 // I Knew You Were Trouble
-  ],
+  // "Taylor Swift": [
+  //   // 74443483, // Style
+  //   // 74443482, // Blank Space
+  //   // 17170438, // Fifteen
+  //   // 74274886, // Shake It Off
+  //   // 31326753 // I Knew You Were Trouble
+  // ],
   // 118
   Queen: [
-    33389569, // Somebody to Love
+    // 33389569, // Somebody to Love
     159561884, // Bohemian Rhapsody
-    85219179, // We Will Rock You
-    77061562, // We Are The Champions
-    31508972 // Another One Bites The Dust
+    // 85219179, // We Will Rock You
+    // 77061562, // We Are The Champions
+    31508972, // Another One Bites The Dust
   ],
   // 33491916
-  "Justin Bieber": [
-    99575547, // Sorry
+  'Justin Bieber': [
+    // 99575547, // Sorry
     17957946, // Boyfriend
-    99575546, // What do you mean?
-    16103716, // Mistletoe
-    32184425 // Baby
+    // 99575546, // What do you mean?
+    // 16103716, // Mistletoe
+    // 32184425 // Baby
   ],
   // 346898
   Adele: [
-    18470707, // Rolling in the Deep
+    // 18470707, // Rolling in the Deep
     84213309, // Hello
-    18470717, // Someone Like You
-    18470711, // Set Fire To The Rain
-    84213314 // Water Under The Bridge
+    // 18470717, // Someone Like You
+    // 18470711, // Set Fire To The Rain
+    84213314, // Water Under The Bridge
   ],
   // 33111847
-  "Ed Sheeran": [
-    73109263, // Thinking Out Loud
+  'Ed Sheeran': [
+    // 73109263, // Thinking Out Loud
     124985078, // Perfect
     124985077, // Shape Of You
-    73109257, // Photograph
-    124985075 // Castle On The Hill
+    // 73109257, // Photograph
+    124985075, // Castle On The Hill
   ],
   // 378462
-  "Lady Gaga": [
+  'Lady Gaga': [
     158988975, // Lady Gaga
-    30971160, // Born This Way
-    15288003, // Bad Romance
+    // 30971160, // Born This Way
+    // 15288003, // Bad Romance
     115127174, // Perfect Illusion
-    115073692 // Million Reasons
+    115073692, // Million Reasons
   ],
   // 18927
   Beyoncé: [
-    31279093, // Halo
+    // 31279093, // Halo
     32158732, // Crazy In Love
-    30202781, // Irreplaceable
+    // 30202781, // Irreplaceable
     35773884, // Drunk In Love
-    53579779 // Single Ladies
+    // 53579779 // Single Ladies
   ],
   // 426
   Eminem: [
-    31301158, // Not Afraid
-    1809819, // Lose Yourself
-    17141769, // Mockingbird
+    // 31301158, // Not Afraid
+    // 1809819, // Lose Yourself
+    // 17141769, // Mockingbird
     157739274, // Killshot
-    16916074 // The Real Slim Shady
-  ]
+    // 16916074 // The Real Slim Shady
+  ],
 };
-
-const testLyrics = [
-  "Thought I'd end up with Sean",
-  "But he wasn't a match",
-  "Wrote some songs about Ricky",
-  "Now I listen and laugh",
-  "Even almost got married",
-  "And for Pete, I'm so thankful",
-  "Wish I could say \"thank you\" to Malcolm",
-  "'Cause he was an angel",
-  "",
-  "One taught me love",
-  "One taught me patience",
-  "And one taught me pain",
-  "Now, I'm so amazing",
-  "Say I've loved and I've lost",
-  "But that's not what I see",
-  "So, look what I got",
-  "Look what you taught me",
-  "And for that, I say",
-  "",
-  "Thank you, next (next)",
-  "Thank you, next (next)",
-  "Thank you, next",
-  "I'm so fuckin' grateful for my ex",
-  "Thank you, next (next)",
-  "Thank you, next (next)",
-  "Thank you, next (next)",
-  "I'm so fuckin'...",
-  "",
-  "Spend more time with my friends",
-  "I ain't worried 'bout nothin'",
-  "Plus, I met someone else",
-  "We havin' better discussions",
-  "...",
-  "",
-  "******* This Lyrics is NOT for Commercial use *******",
-  "(1409618061236)"
-];
